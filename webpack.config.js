c214f5a956de4fb76c14d21631f1bca10cfc017b
Agata Urbańska <agata@agatau.home>
2020-05-18 14:46:27 +0200

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const entryPath = "App";
+const entryFile = "App.js";
+
+module.exports = {
+  entry: ["whatwg-fetch", `./${entryPath}/js/${entryFile}`],
+  output: {
+    filename: "out.js",
+    path: path.resolve(__dirname, `${entryPath}/build`)
+  },
+  devServer: {
+    contentBase: path.join(__dirname, `${entryPath}`),
+    publicPath: "/build/",
+    compress: true,
+    port: 3005,
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+};
